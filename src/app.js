160f8bba438971441af02bdea434938bab2e2794
Maximo Aranibe <darknesswong@gmail.com>
2023-03-04 10:58:09 -0300

    Agregando cambio

--- src/app.js
+++ src/app.js
@@ -22,20 +22,20 @@ app.get('/products', async (req, res) => {
 	} else {
 		res.send({productLimit})
 	}
-	/*   if (!limit) {
-    res.send({ products });
-  } else {
-    let productsLimit = products.slice(1, limit);
-
-    res.send(productsLimit);
-  } */
 });
 
 app.get('/products/:pid', async (req,res) => {
 	const pid = req.params.pid;
 	const products = await manager.read();
-	const productFind = products.filter(e=> e.id == pid);
-	res.send(productFind)
+	const productFind = products.find(e => e.id == pid);
+	
+	if(!productFind){
+		return res
+		.status(404)
+		.send({error: `No existe el usuario con ID ${req.params.pid}`})
+	}else{
+		res.json({productFind})
+	}
 })
 
 /* app.get("/productos", async (req, res) => {
